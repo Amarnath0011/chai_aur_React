588e01e6da5dfc99e878e3371c14385ffab07205
Amarnath0011 <amarnathkumar9305@gmail.com>
2025-04-19 11:12:46 +0530

    first and second project of react

---
+++ 01create-app/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+   <h1>Chai aur code project using create-project || Amarnath</h1>
+  );
+}
+
+export default App;
---
+++ vite-app/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  
+
+  return (
+   <h1>chai aur react with vite || Amarnath</h1>
+  )
+}
+
+export default App
